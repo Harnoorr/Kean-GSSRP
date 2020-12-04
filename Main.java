e938a39af637092b8eb54b53ccb5c5785a322454
camomochi <73672810+camomochi@users.noreply.github.com>
2020-12-04 09:08:10 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,49 @@
-import java.awt.Image;
-
-import javax.swing.ImageIcon;
+import java.awt.Component;
+import java.awt.Dimension;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
-
+import javax.swing.JPanel;
 public class Main {
-
-	public static void main(String[]args) {
+	
+	/**
+	 * Creates a GUI inside a JFrame window
+	 * this method is initialized in the main method
+	*/
+	public static void createGUI() {
 		
-		//Creates a 800x600 Window
+		//Initialize JFrame
 		JFrame frame = new JFrame();
-		frame.setSize(800,600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+		
+		//Initialize JPanel
+		JPanel panel = new JPanel();
+		JButton btn1 = new JButton("Increase");
+		JButton btn2 = new JButton("Decrease");
+		
+		//Adds 2 buttons to the JPanel
+		panel.add(btn1);
+		panel.add(btn2);
+		
+		//JPanel Settings
+		panel.setAlignmentX(Component.LEFT_ALIGNMENT);
+		panel.setPreferredSize(new Dimension(100, 500));
+		panel.setMaximumSize(new Dimension(100, 500));
+		panel.setBorder(BorderFactory.createTitledBorder("Caliper"));
+		
+		//Adds JPanel to JFrame
+		frame.getContentPane().add(panel);
+		frame.setSize(800, 600);
 		frame.setVisible(true);
 		frame.setTitle("Caliper Program");
 		
-    		//Creates an ImageIcon object called "caliper"
-		ImageIcon caliper = new ImageIcon("caliper.jpg");
-		Image logo = caliper.getImage();
-		frame.setIconImage(logo);
-		
-		frame.setResizable(false);
-
 	}
 	
+	public static void main(String[] args) {
+		
+		createGUI();
+		
+	}
 }
