f5addaf6eb0edd242c905e7f94bce8068b68433b
camomochi <73672810+camomochi@users.noreply.github.com>
2020-12-23 09:22:58 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,57 +1,40 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-public class Main{
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Image;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Main {
 	
-	Button btn1;
-	Button btn2;
 	static double value = 0;
 	
-	/**
-	 * 
-	 * Creates a JComponent that includes Graphics to be 
-	 * used as an argument for JPanel
-	 *
-	 */
-	public class MyGraphics extends JComponent {
-		
-
-		private static final long serialVersionUID = 1L;
-
-		MyGraphics(){
-			setPreferredSize(new Dimension(800, 600));
-		}
-		
-		@Override
-		public void paintComponent(Graphics g) {
-			g.fillRect(80,  80,  100,  100);
-		}
-	}
+	public void addComponentsToPane(Container pane) {
+	    pane.setLayout(null);
+	    pane.setBackground(Color.LIGHT_GRAY);
+	    
+	    //Buttons 
+	    JButton b1 = new JButton("Increase");
+	    JButton b2 = new JButton("Decrease");
 	
-	
-	
-	/**
-	 * 
-	 * Creates a GUI inside a JFrame window
-	 * this method is initialized in the main method
-	 * 
-	*/
-	public void createGUI() {
-		
-		//Initialize JFrame
-		final JFrame frame = new JFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new GridLayout(0, 2));
-		
-		//Initialize JPanel
-		JPanel panel = new JPanel();
-		JPanel objectPanel = new JPanel();
-		JPanel caliperPanel = new JPanel();
-		JButton btn1 = new JButton("Increase");
-		JButton btn2 = new JButton("Decrease");
-		
-		//KeyListeners for Counter
-		btn1.addActionListener(new ActionListener() {
+	    pane.add(b1);
+	    pane.add(b2);
+	    Insets insets = pane.getInsets();
+	    Dimension size = b1.getPreferredSize();
+	    b1.setBounds(25 + insets.left, 5 + insets.top, size.width, size.height);
+	    size = b2.getPreferredSize();
+	    b2.setBounds(25 + insets.left, 40 + insets.top, size.width, size.height);
+	    
+	    //Action Listeners for Buttons
+	    b1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				value = value + 0.1;
 				double roundOff = Math.round(value*100)/100D;
@@ -59,7 +42,7 @@ public class Main{
 			}
 		});
 		
-		btn2.addActionListener(new ActionListener() {
+		b2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				value = value + -0.1;
 				double roundOff = Math.round(value*100)/100D;
@@ -68,48 +51,48 @@ public class Main{
 		});
 		
 		
-		//Adds 2 buttons to the JPanel
-		panel.add(btn1);
-		panel.add(btn2);
-		
-		/*
-		//JPanel Settings
-		panel.setAlignmentX(Component.LEFT_ALIGNMENT);
-		panel.setPreferredSize(new Dimension(100, 500));
-		panel.setMaximumSize(new Dimension(100, 500));
-		panel.setBorder(BorderFactory.createTitledBorder("Caliper"));
-		*/
-		
-		//Adds JPanels to JFrame
-		frame.getContentPane().add(panel);
-		frame.add(caliperPanel);
-		frame.getContentPane().add(objectPanel);
-		frame.setSize(1920, 1080);
-		frame.setVisible(true);
-		frame.setTitle("Caliper Program");
-		
-		//Adds Graphics to objectPanel
-		objectPanel.add(new MyGraphics());
-		
-		//Adds Caliper Image to caliperPanel
-		JLabel imageLabel = new JLabel();
-		imageLabel.setIcon(new ImageIcon("Insert File Path to Caliper Body"));
-		caliperPanel.add(imageLabel);
-		
-	}
+		//Experimental JPanel for Cube, feel free to improve
+		JPanel objectPanel = new JPanel();
+	    pane.add(objectPanel);
+	    objectPanel.setBounds(100 + insets.left, 500 + insets.top, 100, 100);
+	    objectPanel.setBackground(Color.YELLOW);
+  }
+
+	/*
+	 *Creates the GUI to be invoked from the main thread 
+	 */
+	private void createAndShowGUI() {
+	    
+	    //Create and set up the window.
+	    JFrame frame = new JFrame("Caliper Program 2.01");
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	
+	    //Set up the content pane.
+	    addComponentsToPane(frame.getContentPane());
 	
+	    //Size and display the window.
+	    Insets insets = frame.getInsets();
+	    frame.setSize(900 + insets.left + insets.right, 900 + insets.top
+	        + insets.bottom);
+	    frame.setVisible(true);
+	    
+	    //Caliper Image 
+	    JLabel label = new JLabel();
+	    Image img = new ImageIcon(this.getClass().getResource("/Caliper Body.png")).getImage();
+	    label.setIcon(new ImageIcon(img));
+	    label.setBounds(0, 53, 840, 293);
+	    frame.getContentPane().add(label);
+  }
+
 	public static void main(String[] args) {
 		
-		//Overrides static instance to run createGUI() in main 
 		EventQueue.invokeLater(new Runnable() {
 			
 			@Override
 			public void run() {
 				Main GUI = new Main();
-				GUI.createGUI();
+				GUI.createAndShowGUI();
 			}
 		});
-		
-	}
+  }
 }
